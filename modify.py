002cf4a627a62349df5272b286dc9e0af6ca83ee
safwanelmadani <safwanelmadani@email.arizona.edu>
2020-06-01 23:52:32 -0700

    update

--- modify.py
+++ modify.py
@@ -4,6 +4,7 @@ import subprocess
 import datetime as dt
 import csv
 import shutil
+import time
 
 os.system("date")
 # os.system("ils")
@@ -26,34 +27,26 @@ def processFileName(arg):
 
 arg=sys.argv[1]
 listOfNewAndOldTimes=processFileName(arg)
-#print(listOfNewAndOldTimes)
 newFilename=arg.replace(listOfNewAndOldTimes[1], listOfNewAndOldTimes[0])
-print( arg, '\n',newFilename)
+print( arg,'\n',newFilename)
 # dest = shutil.copyfile(arg, newFilename) 
 # print("New file was created at ", dest)
-subprocess.run('cp '+arg+' '+ newFilename, shell=True) # cp the original file with a new name
-removeFileExt=arg.replace('.csv','')
-subprocess.run('echo '+'"original filename: '+arg[-54:]+'"'+ ' >> '+ removeFileExt+'_back_in_time.txt', shell=True) # create a file named {ORIGINALFILENAME}_back_in_time.txt
-#whose contents are the original filename, and the new file name
-subprocess.run('echo '+'"new filename: '+newFilename[-54:]+'"'+ ' >> '+ removeFileExt+'_back_in_time.txt', shell=True)
+# subprocess.run('cp '+arg+' '+ newFilename, shell=True) # cp the original file with a new name
+# removeFileExt=arg.replace('.csv','')
+# subprocess.run('echo '+'"original filename: '+arg[-54:]+'"'+ ' >> '+ removeFileExt+'_back_in_time.txt', shell=True) # create a file named {ORIGINALFILENAME}_back_in_time.txt
+# #whose contents are the original filename, and the new file name
+# subprocess.run('echo '+'"new filename: '+newFilename[-54:]+'"'+ ' >> '+ removeFileExt+'_back_in_time.txt', shell=True)
+
+
+ts=float("1590893701.741696")-(3600*7)
+
+p1=subprocess.check_output('date -d @'+str(ts)+' '+'"+%Y-%m-%d-%H-%M-%S"', shell=True)
+newTime=p1.decode().strip('\n')
+print(newTime)
+
+filename=newFilename
 
 
 
 
 
-
-# ts = float("1590893701.741696") # time to read from file
-
-# # if you encounter a "year is out of range" error the timestamp
-# # may be in milliseconds, try `ts /= 1000` in that case
-# timeStr=dt.datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S') # converting unix to readable date
-# print(timeStr) # print data
-
-
-
-
-# print ('Current date/time: {}'.format(dt.datetime.utcfromtimestamp(ts)-hours_removed)) # adding hours to the unix time 
-
-# fomatting=dt.datetime.utcfromtimestamp(ts)-hours_removed
-
-# print(fomatting.strftime('%Y-%m-%d-%H-%M-%S'))
